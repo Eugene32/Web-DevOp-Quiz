dce5bec7a731a93f23deee6b61b0220eefc1156e
Reynan Kanindot <ekahiyang@gmail.com>
2021-12-14 10:56:07 +1100

    moved questions obj to questions.js and completed high scores sorting.

---
+++ assets/js/questions.js
@@ -0,0 +1,94 @@
+
+
+var QuestionList = [
+
+    question1 = {
+        question: "HTML stands for what?",
+        choice1: "A. Hyper-Toll Mocking Language",
+        choice2: "B. Hyperberic Token Logrithm",
+        choice3: "C. Hypothetical Markup Language",
+        choice4: "D. Hypertext Markup Language",
+        answer: "D. Hypertext Markup Language"
+    },
+
+    question2 = {
+        question: "Which statement inserts an image?",
+        choice1: "A. Img file = 'pic.jpg'",
+        choice2: "B. img src = pic.jpg",
+        choice3: "C. Picture = pic.jpg",
+        choice4: 'D. img src = "pic.jpg"',
+        answer: 'D. img src = "pic.jpg"'
+    },
+
+    question3 = {
+        question: "Which tag should you use to make a numbered list?",
+        choice1: "A. li",
+        choice2: "B. ul",
+        choice3: "C. ol",
+        choice4: "D. ls",
+        answer: "C. ol"
+    },
+
+    question4 = {
+        question: "What does CSS stand for?",
+        choice1: "A. Cascading Style Sheets",
+        choice2: "B. Colorful Style Sheets",
+        choice3: "C. Computer Style Sheets",
+        choice4: "D. Coding Syntax System",
+        answer: "A. Cascading Style Sheets"
+    },
+
+    question5 = {
+        question: "How do you remove the underline in a hyperlink display?",
+        choice1: "A. a {text-decoration: no_underline}",
+        choice2: "B. a {text-decoration: none}",
+        choice3: "C. a {text-underline: none}",
+        choice4: "D. a {underline: none}",
+        answer: "B. a {text-decoration: none}"
+    },
+
+    question6 = {
+        question: "Which Of The Dialog Box Display a Message And a Data Entry Field?",
+        choice1: "A. Alert()",
+        choice2: "B. Prompt()",
+        choice3: "C. Confirm()",
+        choice4: "D. Msg()",
+        answer: "B. Prompt()"
+    },
+
+    question7 = {
+        question: "Javascript has an extension of: ___________ ",
+        choice1: "A. jscript",
+        choice2: "B. jvspt",
+        choice3: "C. scpt",
+        choice4: "D. js",
+        answer: "D. js"
+    },
+
+    question8 = {
+        question: "What do you call a function inside an object?",
+        choice1: "A. Link",
+        choice2: "B. Element",
+        choice3: "C. Method",
+        choice4: "D. Key",
+        answer: "C. Method"
+    },
+
+    question9 = {
+        question: 'Which event occurs when the user clicks on an HTML element?',
+        choice1: "A. onmouseclick",
+        choice2: "B. onclick",
+        choice3: "C. click",
+        choice4: "D. clickdown",
+        answer: "B. onclick"
+    },
+
+    question10 = {
+        question: "Which place or portion in the HTML is the best to place the JavaScript",
+        choice1: "A. head",
+        choice2: "B. footer",
+        choice3: "C. end of body",
+        choice4: "D. start of body",
+        answer: "C. end of body"
+    }
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,102 +13,6 @@ var scoreListPointer;
 
 
 
-
-
-
-var QuestionList = [
-
-    question1 = {
-        question: "HTML stands for what?",
-        choice1: "A. Hyper-Toll Mocking Language",
-        choice2: "B. Hyperberic Token Logrithm",
-        choice3: "C. Hypothetical Markup Language",
-        choice4: "D. Hypertext Markup Language",
-        answer: "D. Hypertext Markup Language"
-    },
-
-    question2 = {
-        question: "Which statement inserts an image?",
-        choice1: "A. Img file = 'pic.jpg'",
-        choice2: "B. img src = pic.jpg",
-        choice3: "C. Picture = pic.jpg",
-        choice4: 'D. img src = "pic.jpg"',
-        answer: 'D. img src = "pic.jpg"'
-    },
-
-    question3 = {
-        question: "Which tag should you use to make a numbered list?",
-        choice1: "A. li",
-        choice2: "B. ul",
-        choice3: "C. ol",
-        choice4: "D. ls",
-        answer: "C. ol"
-    },
-
-    question4 = {
-        question: "What does CSS stand for?",
-        choice1: "A. Cascading Style Sheets",
-        choice2: "B. Colorful Style Sheets",
-        choice3: "C. Computer Style Sheets",
-        choice4: "D. Coding Syntax System",
-        answer: "A. Cascading Style Sheets"
-    },
-
-    question5 = {
-        question: "How do you remove the underline in a hyperlink display?",
-        choice1: "A. a {text-decoration: no_underline}",
-        choice2: "B. a {text-decoration: none}",
-        choice3: "C. a {text-underline: none}",
-        choice4: "D. a {underline: none}",
-        answer: "B. a {text-decoration: none}"
-    },
-
-    question6 = {
-        question: "Which Of The Dialog Box Display a Message And a Data Entry Field?",
-        choice1: "A. Alert()",
-        choice2: "B. Prompt()",
-        choice3: "C. Confirm()",
-        choice4: "D. Msg()",
-        answer: "B. Prompt()"
-    },
-
-    question7 = {
-        question: "Javascript has an extension of: ___________ ",
-        choice1: "A. jscript",
-        choice2: "B. jvspt",
-        choice3: "C. scpt",
-        choice4: "D. js",
-        answer: "D. js"
-    },
-
-    question8 = {
-        question: "What do you call a function inside an object?",
-        choice1: "A. Link",
-        choice2: "B. Element",
-        choice3: "C. Method",
-        choice4: "D. Key",
-        answer: "C. Method"
-    },
-
-    question9 = {
-        question: 'Which event occurs when the user clicks on an HTML element?',
-        choice1: "A. onmouseclick",
-        choice2: "B. onclick",
-        choice3: "C. click",
-        choice4: "D. clickdown",
-        answer: "B. onclick"
-    },
-
-    question10 = {
-        question: "Which place or portion in the HTML is the best to place the JavaScript",
-        choice1: "A. head",
-        choice2: "B. footer",
-        choice3: "C. end of body",
-        choice4: "D. start of body",
-        answer: "C. end of body"
-    }
-]
-
 // Event Listeners
 
 document.getElementById('highScoreWindow').addEventListener('click', displayHighScore);
@@ -247,7 +151,7 @@ function displayResult(selectedAnswer) {
         WindowforResult.style.color = 'green';
         console.log("Display:  The answer is correct");
     }
-    setTimeout (deleteResult, 1000);
+    setTimeout(deleteResult, 1000);
 }
 
 
@@ -305,13 +209,21 @@ function saveScore() {
             highScoreList = JSON.parse(tempStringList);
         }
         highScoreList.push(objScore);
+
+        //Sorting the high scores from highest to lowest
+        highScoreList.sort((a, b) => {return b.Score - a.Score;});
+
+        // Saving a string file into the local storage
         localStorage.setItem("list", JSON.stringify(highScoreList));
         displayHighScore();
     }
     else {
+        // Prompt as the input is blank
         alert("Please enter your initials!");
     }
 }
+
+
 function displayHighScore() {
     clearAllDisplay();
     var tempStringList = localStorage.getItem("list");
@@ -330,8 +242,8 @@ function displayHighScore() {
         listSpan.innerHTML = x + 1 + ". " + highScoreList[x].Init + " - " + highScoreList[x].Score;
         WindowforChoices.style.textAlign = "left";
         WindowforChoices.style.position = "relative";
-        WindowforChoices.style.left = "38%";
-        WindowforChoices.style.width = "49%";
+        WindowforChoices.style.left = "37.5%";
+        WindowforChoices.style.width = "50%";
 
         WindowforChoices.appendChild(listSpan);
     }
@@ -360,6 +272,13 @@ function displayOptions() {
     clearBtn.setAttribute("class", "btn-start");
     clearBtn.innerHTML = "CLEAR HIGHSCORES";
     var clearButton = document.getElementById("btn-clearHighScore");
+    document.getElementById("btn-clearHighScore").addEventListener('click', clearHighScores);
+
+}
+
+function clearHighScores() {
+    localStorage.clear();
+    displayHighScore()
 
 }
 
@@ -396,7 +315,7 @@ function clearForNextQuestion() {
 
 }
 
-function deleteResult () {
+function deleteResult() {
     while (WindowforResult.firstChild) {
         WindowforResult.removeChild(WindowforResult.firstChild);
     }
